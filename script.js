7ccacee6eb76efda8ca98d89c02315d26fd75c63
Clément Boulanger <clement.boulangerpro@gmail.com>
2024-01-26 14:23:59 +0100

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-drab-seven.vercel.app/')
+fetch('https://whatyearisit-backend-drab-seven.vercel.app/year')
 .then(response => response.json())
 .then(data => {
     document.querySelector('#year').textContent = data.year
